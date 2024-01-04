86b5eac36f25e1514601e19217316079f1a52649
Alondra Gerke <141412967+alondragerke@users.noreply.github.com>
2024-01-04 13:33:52 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,34 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let socialMedia = "";
+  if (variables.twitter)
+    socialMedia += `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`;
+  if (variables.github)
+    socialMedia += `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`;
+  if (variables.linkedin)
+    socialMedia += `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`;
+  if (variables.instagram)
+    socialMedia += `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`;
+
+  const socialMediaPositionClass =
+    variables.socialMediaPosition === "left"
+      ? "position-left"
+      : "position-right";
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${
+            variables.city
+              ? `${variables.city}, ${variables.country}`
+              : variables.country
+          }</h3>
+          <ul class="${socialMediaPositionClass}">
+            ${socialMedia}
           </ul>
         </div>
     `;
@@ -54,7 +70,8 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://t3.ftcdn.net/jpg/06/24/12/90/240_F_624129005_8dpKDb25iwxMCXc8JTHPshuEry7fcdmc.jpg",
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
@@ -64,11 +81,11 @@ window.onload = function() {
     github: null,
     linkedin: null,
     instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    name: "Lucy",
+    lastName: "Boilett",
+    role: "Web Developer",
+    country: "USA",
+    city: "Miami"
   };
   render(window.variables); // render the card for the first time
 
@@ -85,6 +102,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
+
       render(Object.assign(window.variables, values)); // render again the card with new values
     });
   });
